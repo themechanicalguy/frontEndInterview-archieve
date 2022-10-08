83e5530025526e42c1c493a3d380463259e1f99c
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-10-08 09:21:42 +0530

    08-10-22

--- JavaScript/8-OOP-with-JS/6-ES6-Classes.js
+++ JavaScript/8-OOP-with-JS/6-ES6-Classes.js
@@ -32,3 +32,35 @@ saurav.greet(); // Hey Saurav
 // 1. Classes are not hoisted
 // 2. Classes are first-class citizen
 // 3. Classes are executed in strict mode.
+
+// EXAMPLE:
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+console.log(acc1.pin); //this should not be accessible
+console.log(acc1.getMovements());
+onsole.log(acc1.movements);
