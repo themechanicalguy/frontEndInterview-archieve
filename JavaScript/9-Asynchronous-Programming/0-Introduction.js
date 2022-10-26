4c64fdace169ade629bedc4c6efe0b3ee3ef940c
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-10-26 10:42:19 +0530

    Update 0-Introduction.js

--- JavaScript/9-Asynchronous-Programming/0-Introduction.js
+++ JavaScript/9-Asynchronous-Programming/0-Introduction.js
@@ -1,4 +1,19 @@
 // JavaScript is a synchronous single-threaded language.
+// What is the difference between Synchronous Code and Asynchronous code?
+/*
+Synchronous:- 
+ * Synchronous code are executed line by line.
+ * Each line of code waits for previous line to finish
+ * Long running operations block the code execution. i.e alert()
+ Ex:
+ 
+Asynchronous:-
+* These code is executed after a task that runs in the background finishes.
+* These code is non-blocking.
+* Execution doesn't wait for an asynchronous task to finish its work.
+* Callback function alone doesn't alone make code asynchronous
+*/
+
 console.log("Before For loop execution");
 for (var i = 0; i < 2; i++) {
   console.log("setTimeout message");
@@ -49,7 +64,7 @@ setTimeout message
 Some of the real-time situations where you may need to use the JavaScript Asynchronous 
 code of execution while implementing business logic are:
 
- * To make an HTTP request call.
+ * To make an HTTP request call. AJAX Call
  * To perform any input/output operations.
  * To deal with client and server communication.
 
