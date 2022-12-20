8c04e098abd051d861750e64bb0267ee34780a5c
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-12-20 17:56:15 +0530

    updated

--- JavaScript/9-Asynchronous-Programming/0-Introduction.js
+++ JavaScript/9-Asynchronous-Programming/0-Introduction.js
@@ -15,7 +15,7 @@ Asynchronous:-
 */
 
 console.log("Before For loop execution");
-for (var i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
   console.log("setTimeout message");
   func1();
   func2();
@@ -39,7 +39,7 @@ function func2() {
  * After For loop execution
  */
 console.log("Before For loop execution");
-for (var i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
   setTimeout(function () {
     console.log("setTimeout message");
     func1();
--- JavaScript/9-Asynchronous-Programming/2.1-consuming-promise.js
+++ JavaScript/9-Asynchronous-Programming/2.1-consuming-promise.js
@@ -4,6 +4,7 @@
 //using Promise we cannot get rid of callbacks but the code will be more readable and
 // we get rid of callback hell.
 const getCountryData = function (country) {
+  //fetch function returns a Promise
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(function (response) {
       console.log(response);
