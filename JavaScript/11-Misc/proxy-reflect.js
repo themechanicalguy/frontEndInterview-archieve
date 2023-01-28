0b3f230110290399b84e20502645bfa66a14bdf2
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-01-28 22:49:58 +0530

    proxy and reflect

--- JavaScript/11-Misc/proxy-reflect.js
+++ JavaScript/11-Misc/proxy-reflect.js
@@ -1,3 +1,8 @@
+//1- Give an example of proxy
+let target = {};
+let proxy = new Proxy(target, {}); //empty handler
+proxy.test = 5;
+
 const gameSettings = { date: "2019-12-30" };
 const gameSettingsproxy = new Proxy(gameSettings, {
   get: (o, property) => {
