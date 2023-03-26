c3b48ca0ff75fe1022dc05c6257af1c3974b2813
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-03-26 14:57:15 +0530

    updated

--- JavaScript/3-How-DOM-really-works/IntersectionAPI/intersection-observer-api.js
+++ JavaScript/3-How-DOM-really-works/IntersectionAPI/intersection-observer-api.js
@@ -7,16 +7,26 @@
      information has grown. Intersection information is needed for many reasons, such as:
 
     Lazy-loading of images or other content as a page is scrolled.
-    Implementing "infinite scrolling" web sites, where more and more content is loaded and rendered as you scroll, so that the user doesn't have to flip through pages.
+    Implementing "infinite scrolling" web sites, where more and more content is loaded and rendered as you scroll, 
+    so that the user doesn't have to flip through pages.
     Reporting of visibility of advertisements in order to calculate ad revenues.
     Deciding whether or not to perform tasks or animation processes based on whether or not the user will see the result.
 
 Implementing intersection detection in the past involved event handlers and loops calling methods like Element.getBoundingClientRect() 
-to build up the needed information for every element affected. Since all this code runs on the main thread, even one of these can cause performance problems. When a site is loaded with these tests, things can get downright ugly.
+to build up the needed information for every element affected. Since all this code runs on the main thread, even one of these can cause 
+performance problems. When a site is loaded with these tests, things can get downright ugly.
 
-Consider a web page that uses infinite scrolling. It uses a vendor-provided library to manage the advertisements placed periodically throughout the page, has animated graphics here and there, and uses a custom library that draws notification boxes and the like. Each of these has its own intersection detection routines, all running on the main thread. The author of the web site may not even realize this is happening, since they may know very little about the inner workings of the two libraries they are using. As the user scrolls the page, these intersection detection routines are firing constantly during the scroll handling code, resulting in an experience that leaves the user frustrated with the browser, the web site, and their computer.
+Consider a web page that uses infinite scrolling. It uses a vendor-provided library to manage the advertisements placed periodically 
+throughout the page, has animated graphics here and there, and uses a custom library that draws notification boxes and the like.
+ Each of these has its own intersection detection routines, all running on the main thread. The author of the web site may not even 
+ realize this is happening, since they may know very little about the inner workings of the two libraries they are using. 
+ As the user scrolls the page, these intersection detection routines are firing constantly during the scroll handling code, 
+ resulting in an experience that leaves the user frustrated with the browser, the web site, and their computer.
 
-The Intersection Observer API lets code register a callback function that is executed whenever an element they wish to monitor enters or exits another element (or the viewport), or when the amount by which the two intersect changes by a requested amount. This way, sites no longer need to do anything on the main thread to watch for this kind of element intersection, and the browser is free to optimize the management of intersections as it sees fit.
+The Intersection Observer API lets code register a callback function that is executed whenever an element they wish to monitor
+ enters or exits another element (or the viewport), or when the amount by which the two intersect changes by a requested amount. 
+ This way, sites no longer need to do anything on the main thread to watch for this kind of element intersection, and the browser is 
+ free to optimize the management of intersections as it sees fit.
 
 One thing the Intersection Observer API can't tell you: the exact number of pixels that overlap or specifically which ones they are; however, it covers the much more common use case of "If they intersect by somewhere around N%, I need to do something."
  */
