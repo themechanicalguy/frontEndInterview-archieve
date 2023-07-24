698825d9f5770b37995e621912464c9739a0d562
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-07-24 20:20:36 +0530

    updated

--- JS-Output-quiz-BFE/test.js
+++ JS-Output-quiz-BFE/test.js
@@ -1,41 +1,27 @@
-// Given a sorted array, find the no. of pairs which have a sum of K
-// I = [1,4,4,5,5,5,6,6,11], K=11
-// O = 6
-
-//Solve this problem before interview to remember the case of implementation
-
-// Naive Approach: O(n2)
-function naivePairSum(arr, sum) {
-  let count = 0;
-  // Consider all possible pairs and check their sums
-  for (let i = 0; i < arr.length; i++)
-    for (let j = i + 1; j < arr.length - 1; j++)
-      if (arr[i] + arr[j] == sum) count++;
-
-  return count;
-}
-
-console.log(getPairsSum([1, 4, 4, 5, 5, 5, 6, 6, 11], 11));
-
-function getPairsSum(arr, sum) {
-  const freqMap = new Map();
-  //Store count of all elements in map m
-  for (const element of arr) {
-    if (freqMap.has(element)) {
-      freqMap.set(element, freqMap.get(element) + 1);
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (str1, str2) {
+  if (str2.length > str1.length) return -1;
+  let i = 0;
+  let j = 0;
+  while (j < str1.length) {
+    if (str1[j] === str2[i]) {
+      console.log("str1[j], str2[i]", str1[j], str2[i]);
+      i++;
+      j++;
+      if (i === str2.length) return j - i + 1;
     } else {
-      freqMap.set(element, 1);
+      if (i !== 0) i = 0;
+      // j--;
+      j++;
     }
+    // j++;
   }
-  console.log(freqMap);
+  console.log(j);
+  return -1;
+};
 
-  let countTwice = 0;
-  //iterate through each element and increment the count(Notice that every pair is counted twice)
-  for (const element of arr) {
-    if (freqMap.has(sum - element)) countTwice += freqMap.get(sum - element);
-    // if(arr[i], arr[i]) pair satisfies the condition, then we need to ensure that the count is decreased by one such that the
-    // (arr[i],arr[i]) pair is not considered
-    if (sum - element === element) countTwice--;
-  }
-  return countTwice / 2;
-}
+console.log(strStr("mississippi", "issip"));
--- Mock-Interviews/Mock-1/machine-coding.js
+++ Mock-Interviews/Mock-1/machine-coding.js
@@ -1,9 +1,9 @@
 const data = [
-  // "/src/form/text/text.js",
-  // "/src/form/text/multiline.js",
-  // "/src/form/number/number.js",
-  // "/src/utils/string.js",
-  // "/src/utils/date.js",
+  "/src/form/text/text.js",
+  "/src/form/text/multiline.js",
+  "/src/form/number/number.js",
+  "/src/utils/string.js",
+  "/src/utils/date.js",
   "/src/general.js",
 ];
 
@@ -22,41 +22,75 @@ const data = [
   "utils": ["date", "string"]
 }
 
+1- delete src from the splitted string -done
+2- loop over the result array and check if element has (.), if it has dot it is a file
+3- check for it parent folder, by going 1 index backward
+4- if items length is more than 1 then create object else create array
+5- if you find a finle with (.) find its parent and push it over there.
+6- Already existing case
+  1- check in the res obj, if parent folder key exist, if index is [0] it is root parent
+  2- if parent exist go inside that again check if child parent exist or not, if exist push file 
+      name over there
+7- return result
+
+
+
 */
-function convertData(data) {
+function sanitizeData(data) {
   const res = {};
-  if (Array.isArray(data)) {
-    let temp = [];
-    data.forEach((item) => temp.push(item.split("/")));
-
-    temp.forEach((item) => {
-      item.forEach((ele, index, arr) => {
-        if (ele !== "" && ele !== "src") {
-          if (!ele.includes(".")) {
-            // check if the folder exists in res object
-            // if (res[ele]) {
-            //   //check if it is a file or folder
-            //   if (arr[index + 1].includes(".")) {
-
-            //   }
-
-            // }
-            //last element is a file not a folder, so push it to array
-            // if()
-            if (arr[index + 1].includes(".")) {
-              res[arr[index - 1]] = { [ele]: [arr[index + 1].split(".")[0]] };
-              // res[]
-            } else {
-              //else create an object with folder name key
-              res[ele] = {};
-            }
+  const temp = data.map((item) => item.split("/"));
+  temp.forEach((arr) => {
+    arr.forEach((item, index, arr) =>
+      item === "src" ? arr.splice(index, 1) : arr
+    );
+    arr.splice(0, 1);
+  });
+  //check for parent folder and create key value pair
+  temp.forEach((item, index, array) => {
+    item.map((ele, i, arr) => {
+      //this condition works only for root folder
+      if (i === 0) {
+        if (!res[ele]) {
+          if (arr.length > 2) {
+            res[ele] = {};
+          } else if (arr.length === 1) {
+            res[ele.split(".")[0]] = [ele.split(".")[0]];
+          } else {
+            res[ele] = [];
           }
         }
-      });
+      }
+
+      if (i > 0) {
+        if (res[arr[0]] && !ele.includes(".")) {
+          res[arr[0]] = { ...res[arr[0]], [ele]: [] };
+        }
+      }
     });
-    console.log(res);
-  }
-  // return 0;
+  });
+  temp.forEach((item, index, array) => {
+    item.map((ele, i, arr) => {
+      if (ele.includes(".") && i > 0) {
+        const parent = arr[i - 1];
+        const [splitted] = ele.split(".");
+
+        if (res[arr[0]][parent]) {
+          res[arr[0]][parent].push(splitted);
+          res[arr[0]][parent].sort();
+        } else if (res[arr[i - 1]]) {
+          res[arr[i - 1]].push(splitted);
+          res[arr[i - 1]].sort();
+        }
+      }
+    });
+  });
+  // sorting
+  Object.keys(res)
+    .sort()
+    .forEach(function (v, i) {
+      console.log(v, data[v]);
+    });
+  return res;
 }
 
-convertData(data);
+console.log(sanitizeData(data));
